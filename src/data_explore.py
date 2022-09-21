eeb025fea48c54dc5dbc8a1c2c6ce2a90218df80
William Goble <will.goble@gmail.com>
2022-09-21 19:57:13 -0400

    implement train_test_split and model verification

--- src/data_explore.py
+++ src/data_explore.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 file_src = '../data/melb_data.csv'
 df = pd.read_csv(file_src)
@@ -22,11 +24,16 @@ X = df[features]
 print(X.describe())
 print(X.head())
 
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+
 model = DecisionTreeRegressor(random_state=1)
-model.fit(X, y)
+model.fit(train_X, train_y)
 
 # making predictions on the first few rows
-print("Making predictions for the following 5 houses")
-print(X.head())
-print("The predictions are:")
-print(model.predict(X.head()))
+# print("Making predictions for the following 5 houses")
+# print(X.head())
+# print("The predictions are:")
+# print(model.predict(X.head()))
+
+val_predictions = model.predict(val_X)
+print(f'MAE: {mean_absolute_error(val_y, val_predictions)}')
