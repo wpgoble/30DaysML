095eb7845b0d02baaebe4f90ec9f72c75ac9029e
William Goble <will.goble@gmail.com>
2022-09-23 12:17:03 -0400

    adds the ability to create and find closest centroid

--- src/kMeans.py
+++ src/kMeans.py
@@ -10,7 +10,8 @@ Plainly, the algorithm entails the following steps:
     3. For samples i=1 to m in the dataset:
         Assign the closest cluster centroid to X[i]
     4. For cluster k=1 to K:
-        Find new cluster centroids by calculating the mean of the points assigned to cluster k.
+        Find new cluster centroids by calculating the mean of the points 
+        assigned to cluster k.
 """
 
 import numpy as np
@@ -31,6 +32,31 @@ def initialize_random_centroids(K, X):
 def euclidean_distance(x1, x2):
     """
     Calculates and returns the euclidean distance between two vectors x1 and x2
+    This can also be done just using numpy's L2 norm
+    np.linalg.norm(x1 - x2)
     """
     return np.sqrt(np.sum(np.power(x1 - x2, 2)))
 
+def closest_centroid(x, centroids, K):
+    """
+    Finds and returns the index of the closest centroid for a given vector x
+    """
+    distances = np.empty(K)
+
+    for i in range(K):
+        distances[i] = euclidean_distance(centroids[i], x)
+    
+    # returns the index of the lowest distance
+    return np.argmin(distances)
+
+def create_centroid(centroids, K, X):
+    """
+    Returns an array of cluster indices for all the data samples
+    """
+    m, _ = np.shape(X)
+    cluster_idx = np.empty(m)
+
+    for i in range(m):
+        cluster_idx[i] = closest_centroid(X[i], centroids, K)
+    
+    return cluster_idx
