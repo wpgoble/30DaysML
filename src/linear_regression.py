174a376fe2997c6d7e18b902165b518e51cf1c52
William Goble <will.goble@gmail.com>
2022-09-26 20:31:46 -0400

    adds temperature data and starts linear regresssion model

---
+++ src/linear_regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.datasets import make_regression
+
+def main():
+    print("Hello, world")
+
+if __name__ == "__main__":
+    main()
